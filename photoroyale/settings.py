8272323903fede6e5b3d509c1107046727d7d4ca
shaman <vibesintime@gmail.com>
2021-04-02 13:27:08 -0400

    fix server issue

--- photoroyale/settings.py
+++ photoroyale/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app'
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'photoroyale.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BASE_DIR / 'db.sqlite3',
+        'NAME': 'photoroyale',
     }
 }
 
