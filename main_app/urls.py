901c7163bb3a3bcc69145e0f66478e69b25c80b0
shaman <vibesintime@gmail.com>
2021-04-02 14:49:25 -0400

    Setup Auth

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('threads/', views.threads_index, name='index'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Thread
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+class PostCreate():
+  model = Thread
+  fields = '__all__'
+  
+  # This inherited method is called when a
+  # valid cat form is being submitted
+  def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+    return super().form_valid(form)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+def threads_index(request):
+    threads = Thread.objects.filter(user=request.user)
+    return render(request, 'threads/index.html', { 'threads': threads })
