102e20e1626cb3c7db8bddfd8749fda20c6d194e
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-02 15:43:30 -0700

    registering models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Thread)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(ThreadImage)
+admin.site.register(PostImage)
+admin.site.register(ProfileImage)
