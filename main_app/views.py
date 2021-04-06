33067f02ba9c146a3520fb06ff566baef25c1d67
Michael <michael.j.lasota@gmail.com>
2021-04-06 11:58:35 -0700

    Add add_image function

--- main_app/views.py
+++ main_app/views.py
@@ -24,8 +24,25 @@ class ThreadCreate(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        print(self.request.FILES)
+        add_photo(self.request.get('image', None), )
         return super().form_valid(form)
 
+def add_photo(photo_file, object_id, object_type):
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Image.objects.create(content_type=object_type, object_id=object_id)
+        except:
+            return False
+    return True
 
 class ThreadDelete(LoginRequiredMixin, DeleteView):
     model = Thread
